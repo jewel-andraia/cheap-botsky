f431d42bbb74925107e01d2c5249589944eecb75
Jewel Andraia Darger-Sacher <jewel@dargersacher.com>
2023-05-29 14:31:59 -0700

    Update models.js

--- src/cheap-botsky/models.js
+++ src/cheap-botsky/models.js
@@ -2,7 +2,14 @@ function CheapBotskyPoster() {
   this.socialBackend = "abstract";
 }
 
-CheapBotskyPoster.prototype.post = function(message, context) {
-  console.log(`CheapBotskyPoster would have posted this:\n ${message}`)
+CheapBotskyPoster.prototype.authenticate = function(details) {
+  console.log('CheapBotskyPoster would have logged in.');
+  console.debug(details);
+  throw "NotImplemented";
+}
+
+CheapBotskyPoster.prototype.post = function(message) {
+  console.log('CheapBotskyPoster would have posted a message.');
+  console.debug(message);
   throw "NotImplemented";
 }
